aea0c1feb7879e3aa6cfd18c5a7428159e59ec8f
Robbie Love <Robbie@ubiquilife.com>
2020-11-06 00:35:52 +1000

    load rest of laravel for cli version (copied from artisan.php), assign more arguments to variables and push them into laravel config

--- user_upload.php
+++ user_upload.php
@@ -9,9 +9,33 @@ use Garden\Cli\Cli;
 use Garden\Cli\TaskLogger;
 
 //ensure we are installed
-//`composer install`;
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
+`composer install`;
+
+//Load Laravel
+require __DIR__ . '/vendor/autoload.php';
+$app = require_once __DIR__ . '/bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Artisan Application
+|--------------------------------------------------------------------------
+|
+| When we run the console application, the current CLI command will be
+| executed in this console and the response sent back to a terminal
+| or another output device for the developers. Here goes nothing!
+|
+*/
+
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+
+$status = $kernel->handle(
+    $input = new Symfony\Component\Console\Input\ArgvInput,
+    new Symfony\Component\Console\Output\ConsoleOutput
+);
+
+//Get CLI info
+$arguments = collect($argv)->slice(1);
+
 $cli = new Cli();
 $cli->description('Accepts a CSV file as input (see command line directives below) and processes the CSV file. The parsed file data is to be inserted into a MySQL database.')
     ->opt('file:f', 'This is the name of the CSV to be parsed.', TRUE)
@@ -29,6 +53,11 @@ $log = new TaskLogger();
 $args = $cli->parse($argv, TRUE);
 $file = $args->getOpt('file', 'users.csv');
 $create_table = $args->getOpt('create_table', FALSE);
+$dry_run = $args->getOpt('dry_run', FALSE);
+$port = $args->getOpt('port', env('DB_PORT', '3306'));
+$user = $args->getOpt('user', env('DB_USERNAME', 'root'));
+$host = $args->getOpt('host', env('DB_HOST', 'localhost'));
+$password = $args->getOpt('password', env('DB_PASSWORD', '123'));
 
 //Check if the file exists
 if (!file_exists($file)) {
@@ -37,6 +66,18 @@ if (!file_exists($file)) {
     $log->info($file . ' found');
 }
 
+config(['DB_PORT' => $port]);
+config(['DB_USERNAME' => $user]);
+config(['DB_HOST' => $host]);
+config(['DB_PASSWORD' => $password]);
+
+//Force db refresh
+`php artisan:refresh`;
+//$artisan = new Artisan();
+//$artisan::call('migrate:refresh', [
+//    '--force' => TRUE,
+//]);
+
 //Abort if we are just here to make the DB - it was 'made' before...
 if ($create_table) {
     die($log->info('MYSQL users table created (no further action taken)'));
@@ -44,4 +85,23 @@ if ($create_table) {
     $log->info('MYSQL users table created');
 }
 
-//dd($args, $arguments);
+dd(config('DB_PORT'),
+    config('DB_HOST'),
+    config('DB_USERNAME'),
+    config('DB_PASSWORD')
+);
+
+/*
+|--------------------------------------------------------------------------
+| Shutdown The Application
+|--------------------------------------------------------------------------
+|
+| Once Artisan has finished running, we will fire off the shutdown events
+| so that any final work may be done by the application before we shut
+| down the process. This is the last thing to happen to the request.
+|
+*/
+
+$kernel->terminate($input, $status);
+
+exit($status);
