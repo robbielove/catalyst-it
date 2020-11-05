c73f8bb599c8264d75afba256b453fb9e9c7f86c
Robbie Love <Robbie@ubiquilife.com>
2020-11-05 23:01:03 +1000

    include garden cli for easy use of cli arguments, define these arguments, force composer to install dependancies before the actual script part is run.

--- user_upload.php
+++ user_upload.php
@@ -4,8 +4,25 @@
  * Date: 5/11/20
  * Time: 10:03 pm
  */
-//echo "Hello";
+use Garden\Cli\Cli;
+
+//ensure we are installed
+`composer install`;
 require __DIR__.'/vendor/autoload.php';
 $app = require_once __DIR__.'/bootstrap/app.php';
-$arguments = collect($argv);
-dd($arguments);
+$cli = new Cli();
+$arguments = collect($argv)->slice(1);
+
+$cli->description('Accepts a CSV file as input (see command line directives below) and processes the CSV file. The parsed file data is to be inserted into a MySQL database.')
+    ->opt('file:f', 'This is the name of the CSV to be parsed.', true)
+    ->opt('create_table', 'This will cause the MySQL users table to be built (and no further action will be taken)', false)
+    ->opt('dry_run', 'This will be used with the --file directive in case we want to run the script but not insert into the DB. All other functions will be executed, but the database won\'t be altered
+', false)
+    ->opt('port:P', 'Port number to use.', false, 'integer')
+    ->opt('user:u', 'User for login if not current user.', true)
+    ->opt('host:h', 'Connect to host.', true)
+    ->opt('password:p', 'Password to use when connecting to server.');
+
+// Parse and return cli args.
+$args = $cli->parse($argv, true);
+dd($args, $arguments);
