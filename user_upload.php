60c532b4c3068558bcd2392c988dde76cd32baf7
Robbie Love <Robbie@ubiquilife.com>
2020-11-05 22:40:24 +1000

    show the cli arguments as a laravel collection

--- user_upload.php
+++ user_upload.php
@@ -4,8 +4,8 @@
  * Date: 5/11/20
  * Time: 10:03 pm
  */
-echo "Hello";
+//echo "Hello";
 require __DIR__.'/vendor/autoload.php';
-//require __DIR__.'/bootstrap/app.php';
 $app = require_once __DIR__.'/bootstrap/app.php';
-dd($app);
+$arguments = collect($argv);
+dd($arguments);
